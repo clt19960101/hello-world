abd6fa58b3a1e4698f1253318acc78a8a5b5b788
chenlangtao <chenlangtao@tal.com>
2023-08-14 16:08:46 +0800

    提交测试

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(2/0);
         System.out.println("Hello World !");
+        System.out.println("Hello World !");
     }
 }
 
