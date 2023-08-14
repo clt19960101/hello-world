3ceb015716a948ad6eb99f34b2a0f1f48e8447f5
chenlangtao <chenlangtao@tal.com>
2023-08-14 10:07:20 +0800

    转为Maven项目

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author: ChenLangtao
+ * @date: 2023/8/14 09:56
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
